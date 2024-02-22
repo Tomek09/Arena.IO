74468ab64461aa8916598d29c7ac04f28ab2bf47
Tomasz Kierpiec <kierpiec.tomek@gmail.com>
2024-02-22 14:51:40 +0100

    Utility scripts

---
+++ Assets/Scripts/Utilities/ObjectMoverSinus.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Utilities {
+	public class ObjectMoverSinus : MonoBehaviour {
+
+		[Header("Values")]
+		[SerializeField] private Transform _target;
+		[SerializeField] private float _speed;
+		[SerializeField] private float _distance;
+		private Vector3 _startPosition;
+
+		private void Start() {
+			if (_target == null) {
+				_target = transform;
+			}
+
+			_startPosition = _target.position;
+		}
+
+		private void Update() {
+			float newPoint = _startPosition.y + Mathf.Sin(Time.time * _speed) * _distance;
+			_target.position = new Vector3(_target.position.x, newPoint, _target.position.z);
+		}
+	}
+}
---
+++ Assets/Scripts/Utilities/ObjectRotator.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Utilities {
+	public class ObjectRotator : MonoBehaviour {
+
+		[Header("Values")]
+		[SerializeField] private Transform _target;
+		[SerializeField] private Vector3 _direction;
+		[SerializeField] private float _speed;
+
+		private void OnValidate() {
+			_direction.Normalize();
+		}
+
+		private void Start() {
+			if (_target == null) {
+				_target = transform;
+			}
+		}
+
+		private void Update() {
+			_target.eulerAngles += _speed * Time.deltaTime * _direction;
+		}
+	}
+}
